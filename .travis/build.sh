75048879b05a4cdd15fd52c7cbd18b954ee872df
Gabriella Turek <turek@mpi-cbg.de>
2019-05-08 15:09:48 +0200

    Travis: add encrypted GPG signing keypair

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
-sh travis-build.sh
+sh travis-build.sh $encrypted_df8d49f7d896_key $encrypted_df8d49f7d896_iv
