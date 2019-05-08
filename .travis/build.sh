916dfeb03164968144a633e5a7993a757c369330
Gabriella Turek <turek@mpi-cbg.de>
2019-05-08 17:07:34 +0200

    Fiddling with release:prepare target

--- .travis/build.sh
+++ .travis/build.sh
@@ -114,7 +114,8 @@ EOL
 		echo "== Cutting and deploying release version =="
 		version=`mvn org.apache.maven.plugins:maven-help-plugin::evaluate -Dexpression=project.version | grep -v "^\["`
 		version=`echo $version | cut -d'-' -f 1`
-		mvn -B -Dtag=v$version release:prepare
+		# Never mind the development version, it will be overwritten in ij1-builds, but if we don't put it here it'll try to come up with one itself
+		mvn -B release:prepare -Dtag=v$version -DreleaseVersion=$version -DdevelopmentVersion=$version-SNAPSHOT
 		mvn -B release:perform -DconnectionUrl=scm:git:https://github.com/imagej/ImageJA -Dtag=v$version
 		checkSuccess $?
 	else
