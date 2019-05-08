19b5fa5c5618d61889340b11f86f23f74c617ff0
Gabriella Turek <turek@mpi-cbg.de>
2019-05-08 16:39:48 +0200

    Get the version out of the pom

--- .travis/build.sh
+++ .travis/build.sh
@@ -112,7 +112,7 @@ EOL
 	then
 		echo
 		echo "== Cutting and deploying release version =="
-		version=`mvn help:evaluate -Dexpression=project.version | grep -v "^\["`
+		version=`mvn org.apache.maven.plugins:maven-help-plugin::evaluate -Dexpression=project.version | grep -v "^\["`
 		version=`echo $version | cut -d'-' -f 1`
 		mvn -B -Dtag=v$version release:prepare
 		mvn -B release:perform -DconnectionUrl=scm:git:https://github.com/imagej/ImageJA -Dtag=v$version
